a9f1ab761be12884d58bc9fe61518ed9ab5d7c41
Alan Huffman <alanhuffman@gmail.com>
2026-07-07 10:31:33 -0500

    fix: prevent wiki content from overflowing viewport horizontally
    Add min-width: 0 at three levels of the grid/flex containment chain
    to override the implicit min-width: auto default that allowed long
    CodeMirror content to push containers wider than the 1fr grid column.

--- src/apps/codascope/views/WikiBrowser.tsx
+++ src/apps/codascope/views/WikiBrowser.tsx
@@ -392,7 +392,7 @@ export function WikiBrowser() {
         )}
 
         {activeTopicId && !buildLog ? (
-          <div style={{ height: "100%", display: "flex", flexDirection: "column" }}>
+          <div style={{ height: "100%", display: "flex", flexDirection: "column", minWidth: 0 }}>
             <div style={{
               display: "flex",
               alignItems: "center",
@@ -421,7 +421,7 @@ export function WikiBrowser() {
                 {saving ? "Saving…" : "Save"}
               </button>
             </div>
-            <div style={{ flex: 1, minHeight: 0 }}>
+            <div style={{ flex: 1, minHeight: 0, minWidth: 0 }}>
               <MarkdownEditor
                 value={activeTopicContent}
                 onChange={setActiveTopicContent}
